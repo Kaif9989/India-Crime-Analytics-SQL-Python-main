2c3645a9eb0f015a7e71f2f5dd33c84a69b7a7da
Kaif9989 <126450844+Kaif9989@users.noreply.github.com>
2026-09-02 15:36:12 +0530

    Add files via upload

---
+++ SQL code file/Full sql file.sql
@@ -0,0 +1,9 @@
+-- create database Crime;
+use crime;
+show tables;
+
+select * from dim_crime;
+select * from dim_location;
+select * from dim_victim;
+select * from dim_weapon;
+select * from fact_crime;
